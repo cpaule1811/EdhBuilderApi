50d18fb6a983a817cee2c0356d71faeef5bb22bd
Charlie Edwards <charles.edwards1811@gmail.com>
2021-08-14 17:06:33 +1000

    commiting sql query change

--- controllers/commanders.js
+++ controllers/commanders.js
@@ -1,19 +1,12 @@
 const getCommanders = (req, res, db) => {
     
-    db('entrys').select('*')
-    .then(cardlist => {
-        console.log(cardlist.rows[1])
-        let commanders = cardlist.rows.filter(card => {
-            if (card.type.includes("Legendary Creature")) { 
-                return true;
-            }
-            else if (card.type.includes("Planeswalker") && !card.modal.includes("transform") && !card.modal.includes("modal_dfc") && card.oracle_text.includes("can be your commander")) { 
-                return true;
-            }
-        return false;
-        })
-        
-        res.send(commanders);
+    db.raw(`select * from entrys 
+    where "type" like 'Legendary Creature%' or "type" 
+    like 'Legendary Enchantment Creature%' or "oracle_text" like '%can be your commander%'
+    order by "type";`)
+    .then(commanders => { 
+        console.log(commanders)
+        res.send(commanders.rows);
     })
     .catch(error => res.status(400).json("could not get commanders"))
 }
