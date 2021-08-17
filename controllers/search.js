36fadfb53c1d2baca77c415c4062c25fef1e18f4
Charlie Edwards <charles.edwards1811@gmail.com>
2021-08-17 11:08:29 +1000

    fixed search

--- controllers/search.js
+++ controllers/search.js
@@ -1,11 +1,10 @@
 const search = (req, res, db) => {
     const { name } = req.query
     const nameFilter = name.toLowerCase().replace(/'|,|-| /g, "")
-    db.raw(`SELECT * from entrys where LOWER(REPLACE(REPLACE(REPLACE(REPLACE("cardName", '''',''), ',', ''), '-', ''), ' ', '')) = '${nameFilter}'
-    UNION ALL SELECT * FROM entrys WHERE LOWER(REPLACE(REPLACE(REPLACE(REPLACE("cardName", '''',''), ',', ''), '-', ''), ' ', '')) like '%${nameFilter}' 
+    db.raw(`SELECT * FROM entrys WHERE LOWER(REPLACE(REPLACE(REPLACE(REPLACE("cardName", '''',''), ',', ''), '-', ''), ' ', '')) like '%${nameFilter}' 
     or LOWER(REPLACE(REPLACE(REPLACE(REPLACE("cardName", '''',''), ',', ''), '-', ''), ' ', '')) like '${nameFilter}%'
     or LOWER(REPLACE(REPLACE(REPLACE(REPLACE("cardName", '''',''), ',', ''), '-', ''), ' ', '')) like '%${nameFilter}%'
-    limit 4;`)
+    order by LENGTH("cardName") limit 4;`)
     .then(searchedCards => {
          res.json(searchedCards.rows)
     })
