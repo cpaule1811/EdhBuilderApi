6da127cacc26254c52c379831e687ee5fc5d7422
Charlie Edwards <charles.edwards1811@gmail.com>
2021-08-18 13:36:18 +1000

    testing buf

--- controllers/jsonfile.js
+++ controllers/jsonfile.js
@@ -2,7 +2,8 @@ const handleJsonFile = (req, res, fs) => {
     const { file } = req.files
     console.log(file)
     var buf = Buffer.from(file.data)
-    console.log(buf)
+    console.log(json.stringify(buf))
+    console.log(buf.toJSON())
     res.json('success')
 }
 
