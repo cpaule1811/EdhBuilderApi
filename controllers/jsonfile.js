37730fab86f5d1802d86838e76098548a421d5b5
Charlie Edwards <charles.edwards1811@gmail.com>
2021-08-18 15:09:46 +1000

    this to work

--- controllers/jsonfile.js
+++ controllers/jsonfile.js
@@ -1,13 +1,12 @@
-const BJSON = require('buffer-json')
-
 const handleJsonFile = (req, res) => {
     const { file } = req.files
     // const filteredString = file.data.toString().replace('[', "").replace(']', "").split(',')
     // console.log(filteredString[0], filteredString[1], filteredString[2])
     // const jsonObj = BJSON.stringify({ buf: file.data })
-    const jsonData = JSON.parse(file.data.toString())
+    const jsonString = file.data.toString()
+    console.log(jsonString.substring(0,50))
+    const jsonData = JSON.parse(jsonString)
     console.log(jsonData)
-    console.log(jsonData[1])
     res.json('success')
 }
 
