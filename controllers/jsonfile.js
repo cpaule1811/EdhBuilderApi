8e6b9872ef5a219d148c8a00f0b6e8ba30109e16
Charlie Edwards <charles.edwards1811@gmail.com>
2021-08-18 14:35:23 +1000

    json transfer

--- controllers/jsonfile.js
+++ controllers/jsonfile.js
@@ -1,7 +1,10 @@
-const handleJsonFile = (req, res) => {
+const handleJsonFile = (req, res, JSON) => {
     const { file } = req.files
-    const filteredString = file.data.toString().replace('[', "").replace(']', "").split(',')
-    console.log(filteredString[0], filteredString[1], filteredString[2])
+    // const filteredString = file.data.toString().replace('[', "").replace(']', "").split(',')
+    // console.log(filteredString[0], filteredString[1], filteredString[2])
+    const ari = JSON.parse(data.toString())
+    console.log(ari)
+    console.log(ari[0])
     res.json('success')
 }
 
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const nodemailer = require("nodemailer");
 const fileUpload = require("express-fileupload")
 const fs = require('fs')
 const path = require('path')
+const JSON = require('JSON')
 
 const app = express()
 
@@ -58,7 +59,7 @@ app.use(helmet())
 app.use(express.json({limit: "2mb"}))
 app.use(cors({ origin: ['https://edh-builder-luicu.ondigitalocean.app', 'https://edhbuilder.com.au', 'http://localhost:3000'] }))
 
-app.post('/jsonentrys', (req, res) => { jsonFile.handleJsonFile(req, res) })
+app.post('/jsonentrys', (req, res) => { jsonFile.handleJsonFile(req, res, JSON) })
 
 app.get('/', (req, res) => { db.raw("select * from entrys limit 1;").then(item => res.json(item.rows[0])) })
 app.put('/username', sanitize.sanitizeData, auth.requireAuth, (req, res) => { username.handleUsername(req, res, db) })
