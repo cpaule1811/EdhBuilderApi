4048895400935322d0f6427bac14e8d4187c59ff
Charlie Edwards <charles.edwards1811@gmail.com>
2021-08-18 11:09:07 +1000

    fixed returning

--- controllers/newSet.js
+++ controllers/newSet.js
@@ -2,6 +2,7 @@ const updateEntrys = (req, res, db) => {
     const { cardsToAdd } = req.body
     db('entrys')
     .insert(cardsToAdd)
+    .returning('*')
     .onConflict('cardName')
     .merge()
     .then(resp => { 
