e87a05af17ee67e9131897ba043e3f62ef927e61
Charlie Edwards <charles.edwards1811@gmail.com>
2021-08-18 14:47:13 +1000

    converting buffer

--- controllers/jsonfile.js
+++ controllers/jsonfile.js
@@ -1,8 +1,10 @@
-const handleJsonFile = (req, res, JSON) => {
+const BJSON = require('buffer-json')
+
+const handleJsonFile = (req, res) => {
     const { file } = req.files
     // const filteredString = file.data.toString().replace('[', "").replace(']', "").split(',')
     // console.log(filteredString[0], filteredString[1], filteredString[2])
-    const ari = JSON.parse(data.toString())
+    const jsonObj = BJSON.stringify({ buf: file.data })
     console.log(ari)
     console.log(ari[0])
     res.json('success')
