400395ee4747a61aee2d23c59836531be910dbb3
Charlie Edwards <charles.edwards1811@gmail.com>
2021-08-18 13:24:51 +1000

    trying buffer

--- controllers/jsonfile.js
+++ controllers/jsonfile.js
@@ -1,13 +1,8 @@
 const handleJsonFile = (req, res, fs) => {
     const { file } = req.files
-    fs.readFile('/' + file.name, (err, data) => {
-        if (err){ 
-            console.log(err)
-        }
-        else { 
-            console.log(data[0])
-        }
-    })
+    console.log(file)
+    var buf = Buffer.from(file.data)
+    console.log(buf)
     res.json('success')
 }
 
