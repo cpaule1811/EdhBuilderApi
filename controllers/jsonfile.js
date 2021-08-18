bf9c31976989c62b1e35a9c6f0a2e6adf7f78ed8
Charlie Edwards <charles.edwards1811@gmail.com>
2021-08-18 14:29:37 +1000

    testing extraction

--- controllers/jsonfile.js
+++ controllers/jsonfile.js
@@ -1,7 +1,7 @@
 const handleJsonFile = (req, res) => {
     const { file } = req.files
-    console.log(file)
-    console.log(file.data.toString().substring(0,30))
+    const filteredString = file.data.toString().replace('[', "").replace(']', "").split(',')
+    console.log(filteredString[0], filteredString[1], filteredString[2])
     res.json('success')
 }
 
