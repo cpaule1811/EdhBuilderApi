4847953b1a5ef3060751d02934c696efcb003778
Charlie Edwards <charles.edwards1811@gmail.com>
2021-08-18 14:15:32 +1000

    need smaller string

--- controllers/jsonfile.js
+++ controllers/jsonfile.js
@@ -1,7 +1,7 @@
 const handleJsonFile = (req, res) => {
     const { file } = req.files
     console.log(file)
-    console.log(file.data.toString())
+    console.log(file.data.toString().substring(0,30))
     res.json('success')
 }
 
