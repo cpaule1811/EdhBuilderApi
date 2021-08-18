e94b99ed871a199c0a4eb2bf65ec0e410c239ab9
Charlie Edwards <charles.edwards1811@gmail.com>
2021-08-18 16:47:29 +1000

    typo

--- controllers/jsonfile.js
+++ controllers/jsonfile.js
@@ -12,13 +12,12 @@ const handleJsonFile = (req, res, db) => {
            res.json("this worked")
         })
         .catch(err => { console.log(err); res.status(400).json('could not update') })
-    res.json('success')
 }
 
 const cardCases = (jsonData) => {
     let cases = ""
     jsonData.forEach(item => {
-        cases += `WHEN "cardName" = '${item.cardName}' THEN ${item.price}} `
+        cases += `WHEN "cardName" = '${item.cardName}' THEN ${item.price} `
     })
     return cases
 }
