d1902fee98f488bd7dc80815a6b72b4d4e506cb0
Charlie Edwards <charles.edwards1811@gmail.com>
2021-08-23 12:18:54 +1000

    testing performance

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.use(fileUpload({
 app.use(express.urlencoded({extended: false}));
 app.use(helmet())
 app.use(express.json({limit: "2mb"}))
-app.use(cors({ origin: ['https://edh-builder-luicu.ondigitalocean.app', 'https://edhbuilder.com.au'] }))
+app.use(cors({ origin: ['https://edh-builder-luicu.ondigitalocean.app', 'https://edhbuilder.com.au', 'http://localhost:3000'] }))
 
 //site data get requests
 app.get('/requestdeck/:id/:userid', auth.requireAuthDecklist, (req, res) => { getdeck.handleGetdeck(req, res, db) })
